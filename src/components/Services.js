33d42b521d23f5dae7ae853698cda54b7a859432
Michael Bouvette <mjbouvet@amazon.com>
2023-06-07 10:20:51 -0700

    testing deployment

--- src/components/Services.js
+++ src/components/Services.js
@@ -11,7 +11,7 @@ const Services = () => {
                     <div class="box">
                         <div class="circle"><FontAwesomeIcon className="icon" icon={faKeyboard} size="2x"/></div>
                         <h3>Quantitative Finance</h3>
-                        <p>I am currently reseraching quantitative finance as it is a great combination of both my background in mathematics and comptuer science, and my passion for finance.</p>
+                        <p>I am currently reseraching quantitative finance as it is a great combination of both my background in mathematics and comptuer science, and my passion for finance</p>
                     </div>
                 </div>
                 {/* - */}
